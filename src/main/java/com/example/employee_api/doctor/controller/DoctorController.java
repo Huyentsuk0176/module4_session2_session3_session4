ba1dcb63b4180b585cebead12824bc9aea205eb6
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2026-03-09 10:38:49 +0900

    module4_session6_ex02_doctor_get_update

--- src/main/java/com/example/employee_api/doctor/controller/DoctorController.java
+++ src/main/java/com/example/employee_api/doctor/controller/DoctorController.java
@@ -3,7 +3,7 @@ package com.example.employee_api.doctor.controller;
 import com.example.employee_api.doctor.model.entity.Doctor;
 import com.example.employee_api.doctor.service.DoctorService;
 import org.springframework.web.bind.annotation.*;
-
+import java.util.List;
 @RestController
 @RequestMapping("/api/v1/doctors")
 public class DoctorController {
@@ -25,5 +25,17 @@ public class DoctorController {
     public String deleteDoctor(@PathVariable Long id) {
         doctorService.deleteDoctor(id);
         return "Doctor deleted successfully";
+    }
+    // update doctor
+    @PutMapping("/{id}")
+    public Doctor updateDoctor(@PathVariable Long id,
+                               @RequestBody Doctor doctor) {
+        return doctorService.updateDoctor(id, doctor);
+    }
+
+    // get all doctors
+    @GetMapping
+    public List<Doctor> getAllDoctors() {
+        return doctorService.getAllDoctors();
     }
 }
--- src/main/java/com/example/employee_api/doctor/service/DoctorService.java
+++ src/main/java/com/example/employee_api/doctor/service/DoctorService.java
@@ -3,6 +3,8 @@ package com.example.employee_api.doctor.service;
 import com.example.employee_api.doctor.model.entity.Doctor;
 import com.example.employee_api.doctor.repository.DoctorRepository;
 import org.springframework.stereotype.Service;
+import java.util.List;
+
 
 @Service
 public class DoctorService {
@@ -19,5 +21,21 @@ public class DoctorService {
 
     public void deleteDoctor(Long id) {
         doctorRepository.deleteById(id);
+    }
+    public Doctor updateDoctor(Long id, Doctor doctor) {
+
+        Doctor existingDoctor = doctorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Doctor not found"));
+
+        existingDoctor.setDoctorCode(doctor.getDoctorCode());
+        existingDoctor.setFullName(doctor.getFullName());
+        existingDoctor.setSpecialization(doctor.getSpecialization());
+        existingDoctor.setExperienceYears(doctor.getExperienceYears());
+
+        return doctorRepository.save(existingDoctor);
+    }
+
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
     }
 }
